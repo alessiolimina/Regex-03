e835ef67250bdee3ab30af67fefac79ed8bb3711
Alessio Limina <alessio.limina@gmail.com>
2023-01-25 19:56:32 +0100

    Exercise: Regex 3

---
+++ src/it/develhope/regex/Start.java
@@ -0,0 +1,17 @@
+package it.develhope.regex;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        System.out.println("-----------Starting-------------------");
+
+        String s1 = "are you able to climb, are you able to swim or are you able to fly?";
+
+        System.out.println(s1.replaceAll("\sare", "_XYZ"));
+
+        System.out.println("--------------------------------------");
+
+    }
+
+}
